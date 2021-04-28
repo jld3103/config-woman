c716701cecf2459e375e6a7984ba51af454af0db
jld3103 <jld3103yt@gmail.com>
2021-04-28 09:49:45 +0200

    Create empty system config file by default

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ import yaml
 def load_system_config(config_directory):
     file_path = os.path.join(config_directory, 'system.yaml')
     if not os.path.exists(file_path):
+        # Create an empty file to start with
+        with open(file_path, 'w') as file:
+            file.write(yaml.dump({'packages': []}, default_flow_style=False))
         return Config([])
     with open(file_path, 'r') as file:
         document = yaml.load(file.read(), Loader=yaml.FullLoader)
