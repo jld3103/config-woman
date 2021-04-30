dd81c5cb7404e4c7951fb2594d098b3ea9b3c65a
jld3103 <jld3103yt@gmail.com>
2021-04-30 12:51:51 +0200

    Create config directory if it does not exist

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ import yaml
 def load_system_config(config_directory):
     file_path = os.path.join(config_directory, 'system.yaml')
     if not os.path.exists(file_path):
+        if not os.path.exists(config_directory):
+            os.makedirs(config_directory)
         # Create an empty file to start with
         with open(file_path, 'w') as file:
             file.write(yaml.dump({'packages': []}, default_flow_style=False))
