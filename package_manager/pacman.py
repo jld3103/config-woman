e8c2055a2e0bb87d76501b00a8a772bc5fe6cb35
jld3103 <jld3103yt@gmail.com>
2021-04-30 12:34:26 +0200

    Improve speed of pacman install reason queries

--- package_manager/pacman.py
+++ package_manager/pacman.py
@@ -1,3 +1,4 @@
+import functools
 import subprocess
 from package_manager.package_manager import PackageManager
 from root import run_as_root
@@ -14,10 +15,19 @@ class Pacman(PackageManager):
         return packages
 
     def is_package_explicitly_installed(self, package) -> bool:
-        for line in subprocess.check_output(['pacman', '-Qi', package]).decode('utf-8').split('\n'):
+        return package in self._get_explicitly_installed_packages()
+
+    @functools.lru_cache()
+    def _get_explicitly_installed_packages(self) -> [str]:
+        packages = []
+        name = ""
+        for line in subprocess.check_output(['pacman', '-Qi']).decode('utf-8').split('\n'):
+            if line.startswith('Name'):
+                name = line.split(': ')[1]
             if line.startswith('Install Reason'):
-                return 'Explicitly installed' in line
-        return True
+                if 'Explicitly installed' in line:
+                    packages.append(name)
+        return packages
 
     def install_packages(self, packages: [str], no_confirm: bool):
         if no_confirm:
