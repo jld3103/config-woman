ec6f8d761ece9807f50e820a1c1c2e3830081f12
jld3103 <jld3103yt@gmail.com>
2021-05-01 13:49:05 +0200

    Fix config directory from environment variable

--- defaults.py
+++ defaults.py
@@ -4,6 +4,6 @@ import appdirs
 default_config_directory = ""
 
 if os.getenv('CONFIG_DIR') is not None:
-    default_config_directory = os.path.join(os.getenv('CONFIG_DIR'), 'config-woman')
+    default_config_directory = os.path.join(os.getenv('CONFIG_DIR'))
 else:
     default_config_directory = os.path.join(appdirs.user_config_dir(), 'config-woman')
